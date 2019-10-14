168620afaf5065f9c8eb321eec20b6adf01add40
BeetleJewws <43725235+BeetleJewws@users.noreply.github.com>
2019-10-14 18:26:51 +0700

    Update lab1.rb

--- lab1.rb
+++ lab1.rb
@@ -4,25 +4,16 @@ def pomogite (n, array)
     if n == 1
       result
     else
-      temp=result.flat_map{|el|array.select{|t|t != el[-1]}.map{|el2|el+[el2]}}
-      help(array,n-1,temp)
+      temp = result.flat_map{|el| array.map{|el2| [el].flatten(1) + [el2]}}
+      help( array,n-1, temp.reject{|x|x[-1] == x[-2]})
     end
   end
+
   help( array,n, array)
+
 end
 
 
 array = [1, "hehe", [1,2]]
 
 puts pomogite(3,array).inspect
-
-
-# aba
-# abc
-# abd
-# aca
-# acb
-# acd
-# ada
-# adb
-# adc
