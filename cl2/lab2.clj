fc84031c428e540d861b86be1125eaa83b782347
BeetleJewws <43725235+BeetleJewws@users.noreply.github.com>
2019-10-20 15:38:35 +0700

    Update lab2.clj

--- cl2/lab2.clj
+++ cl2/lab2.clj
@@ -1,24 +1,31 @@
-(ns labi.lab2)
+(ns labi.lab2
+  (:import (java.util.logging XMLFormatter)))
 
 
 (defn check-step
   [start, finish, step]
   (if (< (- finish step) start) (- finish start) step))
 
-(defn core [fun, step, start, finish, result]
-  (if (= start finish)
-    (println result)
-    (memoize (core
-               fun
-               step
-               start
-               (- finish (check-step start finish step))
-               (+ (* (/ (+ (fun finish) (fun (- finish (check-step start finish step))))2) (- finish (- finish (check-step start finish step)))) result)
-               ))))
+(defn calc
+  [x0, x1, fun]
+  (* (* (+ (fun x1) (fun x0))0.5) (- x1 x0)))
+
+(def core
+  (memoize
+    (fn [fun, step, start, finish]
+      (println start, finish)
+      (if (= start finish)
+        0
+        (+ (calc (- finish step) finish fun)
+        (core fun step start (- finish (check-step start finish step))))
+        ))))
 
 
-(time (core (fn [el] (* el el)) 0.1 0. 100. 0))
-(time (core (fn [el] (* el el)) 0.1 0. 101. 0))
-(time (core (fn [el] (* el el)) 0.1 0. 102. 0))
-(time (core (fn [el] (* el el)) 0.1 0. 103. 0))
+
+
+(println (core (fn [el] (* el el)) 1. 0. 10.))
+(println (core (fn [el] (* el el)) 1. 0. 10.))
+
+
+
 
