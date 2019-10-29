efc17a12c3626fb1a841426fd94cdd39f7dfb51d
BeetleJewws <43725235+BeetleJewws@users.noreply.github.com>
2019-10-29 13:15:33 +0700

    Update lab3.clj

--- cl3.1/lab3.clj
+++ cl3.1/lab3.clj
@@ -11,19 +11,16 @@
 
 (defn calc
   [x0, x1, fun]
-  (print ".")
   (* (* (+ (fun x1) (fun x0)) 1/2) (- x1 x0)))
 
 (defn core
-  ([x0, x1, fun] (lazy-seq (cons (calc x0 x1 fun) (core x1 (+ x1(- x1 x0)) fun)))))
+  ([x0, x1, fun, res] 
+   (print ".")
+   (lazy-seq (cons (+ res (calc x0 x1 fun)) (core x1 (+ x1(- x1 x0)) fun (+ res (calc x0 x1 fun)))))))
 
 
 
-(def seq (core 0 0.5 myFn))
+(def seq (core 0 0.5 myFn 0))
 
-(println (reduce + (take (finish 2.5) seq)))
-(println (reduce + (take (finish 3.0) seq)))
-
-
-
-
+(println (last (take (finish 2.5) seq)))
+(println (last (take (finish 3.0) seq)))
